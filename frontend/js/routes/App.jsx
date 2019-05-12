906fcab1e591e72821c96d3f676c01325b4edf3d
Kalob Porter <kalob@kalob.net>
2019-05-12 11:25:13 -0400

    Upgrade to react v16.8

--- frontend/js/routes/App.jsx
+++ frontend/js/routes/App.jsx
@@ -1,44 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 
-import purple from '@material-ui/core/colors/purple';
-import green from '@material-ui/core/colors/green';
-import red from '@material-ui/core/colors/red';
+import theme from 'js/theme';
 
-// This is an example theme, feel free to edit to your heart's desire...
-// https://material-ui-1dab0.firebaseapp.com/customization/themes/
-const theme = createMuiTheme({
-  palette: {
-    primary: purple, // Purple and green play nicely together.
-    secondary: {
-      ...green,
-      A400: '#00e677',
-    },
-    error: red,
-  },
-});
-
-export default class App extends Component {
-  static propTypes = {
-    children: PropTypes.oneOfType([PropTypes.array, PropTypes.object]),
-  }
-
-  static defaultProps = {
-    children: [],
-  }
-  render() {
-    return (
-      <MuiThemeProvider theme={theme}>
-        {/* CSS Reset */}
-        <CssBaseline />
-        <div>
-          {this.props.children}
-        </div>
-      </MuiThemeProvider>
-    );
-  }
+export default function App({ children }) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <div>
+        { children }
+      </div>
+    </ThemeProvider>
+  );
 }
+
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+};
--- frontend/js/routes/Home/index.jsx
+++ frontend/js/routes/Home/index.jsx
@@ -1,48 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
 import style from './style';
 
-// Pull in our styles from the styles.js file
-@withStyles(style)
-export default class Home extends Component {
-  static propTypes = {
-    // Be sure to use prop types, typing is important to be sure you are
-    // getting the props that you want
-    classes: PropTypes.object.isRequired,
-  }
-  // Set the default state
-  state = { count: 0 };
+function Home({ classes }) {
+  const [count, setCount] = useState(0);
 
-  // Arrow functions keep context allowing us to call `this`
-  // and know we are reffering to the right thing.
-  incrementCount = () => {
-    const { count } = this.state;
-    // Increment the count
-    this.setState({ count: count + 1 });
-  }
-
-  render() {
-    // Access our styles
-    const { classes } = this.props;
-    // Access the component state
-    const { count } = this.state;
-
-    return (
-      <Grid container className={classes.main}>
-        <Grid item xs={12}>
-          <Typography variant="headline">React Prototype App</Typography>
-          <br />
-          {/* Since this.incrementCounter returns a function we can just pass it in */}
-          <Button variant="raised" onClick={this.incrementCount}>Increment</Button>
-          <Typography variant="subheading">{count}</Typography>
-        </Grid>
+  return (
+    <Grid container>
+      <Grid item xs={12} className={classes.main}>
+        <Typography variant="headline">React Prototype App</Typography>
+        <br />
+        <Button variant="raised" onClick={() => setCount(count + 1)}>Increment</Button>
+        <Typography variant="subheading">{count}</Typography>
       </Grid>
-    );
-  }
+    </Grid>
+  );
 }
+
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(style)(Home);
