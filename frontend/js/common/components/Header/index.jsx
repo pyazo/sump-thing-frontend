4fd6670a2c91fa4a60fbf04c6b524507074f54af
Kalob Porter <kalob@kalob.net>
2019-10-27 12:10:41 -0400

    Fix for header

--- frontend/js/common/components/Header/index.jsx
+++ frontend/js/common/components/Header/index.jsx
@@ -29,7 +29,7 @@ export default function Header({ elevated, text }) {
         </Grid>
         {
           auth.isLoggedIn && !currentUser.loading
-            ? <UserMenu user={currentUser.currentUser} />
+            ? <UserMenu user={currentUser.user} />
             : <UnauthedActions />
         }
       </Grid>
